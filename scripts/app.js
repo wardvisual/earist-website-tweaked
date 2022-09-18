c40676dbfb17cbe0fb1a33709bb5b9a06565133e
wardvisual <wardvisual@gmail.com>
2022-09-18 16:57:32 +0000

    chore: update navbar

---
+++ scripts/app.js
@@ -0,0 +1,34 @@
+const closeMenuButton = document.querySelector("#--close-menu");
+const openMenuButton = document.querySelector("#--open-menu");
+const menu = document.querySelector("#--menu");
+
+class App {
+    constructor() {}
+
+    static hidden = "hidden";
+    static visible = "visible";
+
+    static closeMenu() {
+        closeMenuButton.addEventListener("click", () => {
+            if (menu.classList.contains(this.visible)) {
+                menu.classList.remove(this.visible);
+                menu.classList.add(this.hidden);
+            }
+        });
+    }
+    static openMenu() {
+        openMenuButton.addEventListener("click", () => {
+            if (menu.classList.contains(this.hidden)) {
+                menu.classList.remove(this.hidden);
+                menu.classList.add(this.visible);
+            }
+        });
+    }
+
+    static start() {
+        this.openMenu();
+        this.closeMenu();
+    }
+}
+
+App.start();
